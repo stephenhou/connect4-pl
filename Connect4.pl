495ac8dcb7e335854db19e17a38d08a2b2a5c4c7
Minxing Wang <mc_wang@live.com>
2018-11-22 11:17:10 -0800

    committing playing board

--- Connect4.pl
+++ Connect4.pl
@@ -1,5 +1,5 @@
 % To run it, try:
-% Connect4.
+% play.
 
 % the starting state for every connect4 game
 connect4_start(gameBoard([['*','*','*','*','*','*'],
@@ -9,3 +9,26 @@ connect4_start(gameBoard([['*','*','*','*','*','*'],
 	                      ['*','*','*','*','*','*'],
 	                      ['*','*','*','*','*','*'],
 	                      ['*','*','*','*','*','*']])).
+
+print_board(gameBoard(X)):-
+  write("Choose column 1-7 to place piece"),
+  nl,
+  print_board_rows(X).
+
+print_board_rows([]) :- nl.
+print_board_rows([H|T]):-
+  write("  "),
+  print_board_element(H),
+  nl,
+  print_board_rows(T).
+
+print_board_element([]).
+print_board_element([H|T]) :-
+  write(H),
+  write("   "),
+  print_board_element(T).
+
+play :-
+  connect4_start(X),
+  print_board(X).
+
